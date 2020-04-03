0e14f0814b8c01474b328f0ea2fc1471755f2a1a
Luis Cruz <luisccc99@gmail.com>
2020-04-03 12:06:23 -0700

    working on checking adjacents

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -3,29 +3,31 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private int xBottomSite = 0;
-    private int xTopSite = 0;
-    private int yBottomSite = 0;
-    private int yTopSite = 0;
+    int topSite = 0;
+    int bottomSite = 1;
 
     private boolean[][] site;
-    private int size;
     private int openSites = 0;
     private int N;
     private WeightedQuickUnionUF system;
 
-
+    //i = x, j = y
     public Percolation(int n) {
-        this.N = n + 1;
-        yTopSite = n;
+        N = n + 1;
         site = new boolean[N][N];
-        size = N * N;
+        int size = N * N;
         system = new WeightedQuickUnionUF(size);
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 site[i][j] = false;
+                //connecting bottom and top site with bottom and top rows
+                if (j == 1 || j == N - 1) {
+                    system.union(xyTo1D(i, 1), topSite);
+                    system.union(xyTo1D(i, N - 1), bottomSite);
+                }
             }
         }
+
     }
 
     public void open(int row, int col) {
@@ -35,8 +37,28 @@ public class Percolation {
             site[row][col] = true;
             openSites++;
         }
-        //TODO: implement checking for adjacent open sites
+
     }
+
+    public void checkNeighbors(int row, int col) {
+        int upY = row - 1;
+        int downY = row + 1;
+        int backX = col - 1;
+        int frontX = col + 1;
+
+        if (isOpen(upY, col)) {
+            system.union(xyTo1D(col, row), xyTo1D(col, upY));
+        } else if (isOpen(downY, col)) {
+            system.union(xyTo1D(col, row), xyTo1D(col, downY));
+        } else if (isOpen(row, frontX)) {
+            system.union(xyTo1D(col, row), xyTo1D(frontX, row));
+        } else if (isOpen(row, backX)) {
+            system.union(xyTo1D(col, row), xyTo1D(backX, row));
+        }
+
+
+    }
+
 
     public boolean isOpen(int row, int col) {
         validateIndex(row);
@@ -47,7 +69,10 @@ public class Percolation {
     public boolean isFull(int row, int col) {
         validateIndex(row);
         validateIndex(col);
-        //TODO: finish this section
+        int element = xyTo1D(col, row);
+        if (isOpen(row, col)) {
+            return connected(element, topSite);
+        }
         return false;
     }
 
@@ -56,18 +81,17 @@ public class Percolation {
     }
 
     public boolean percolates() {
-        int topSite = xyTo1D(xTopSite, yTopSite);
-        int bottomSite = xyTo1D(xBottomSite, yBottomSite);
-        if (system.find(topSite) == system.find(bottomSite)) {
-            return true;
-        }
-        return false;
+        return connected(topSite, bottomSite);
+    }
+
+    public boolean connected(int p, int q) {
+        return system.find(p) == system.find(q);
     }
 
     private void validateIndex(int entry) {
         if (entry <= 0 || entry > N) {
             throw new IllegalArgumentException(
-                    "index" + entry + " out of bounds");
+                    "entry " + entry + " out of bounds");
         }
     }
 
@@ -79,8 +103,16 @@ public class Percolation {
     public static void main(String[] args) {
         int N = StdIn.readInt();
         Percolation system = new Percolation(N);
+        system.open(1, 2);
+        system.open(2, 3);
+        system.open(2, 1);
+        system.open(3, 2);
+        System.out.println(system.connected(6, 14));
         system.open(2, 2);
-        system.open(1, 1);
-        System.out.println(system.numberOfOpenSites());
+        system.checkNeighbors(2,2);
+        System.out.println(system.connected(10, 14));
+
+        //TODO: corner and edge cases
+
     }
 }
