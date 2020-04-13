64835b6388b6c5f3031c4bf28ce2ac3aba00a17e
Luis Cruz <luisccc99@gmail.com>
2020-04-12 23:32:54 -0600

    finishing Percolation

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -11,7 +11,7 @@ public class Percolation {
     private int N;
     private WeightedQuickUnionUF system;
 
-    //i = x, j = y
+    //i = y, j = x
     public Percolation(int n) {
         N = n + 1;
         site = new boolean[N][N];
@@ -21,9 +21,9 @@ public class Percolation {
             for (int j = 0; j < N; j++) {
                 site[i][j] = false;
                 //connecting bottom and top site with bottom and top rows
-                if (j == 1 || j == N - 1) {
-                    system.union(xyTo1D(i, 1), topSite);
-                    system.union(xyTo1D(i, N - 1), bottomSite);
+                if (i == 1 || i == N - 1) {
+                    system.union(xyTo1D(j, 1), topSite);
+                    system.union(xyTo1D(j, N - 1), bottomSite);
                 }
             }
         }
@@ -37,6 +37,7 @@ public class Percolation {
             site[row][col] = true;
             openSites++;
         }
+        checkNeighbors(row, col);
 
     }
 
@@ -46,19 +47,48 @@ public class Percolation {
         int backX = col - 1;
         int frontX = col + 1;
 
-        if (isOpen(upY, col)) {
-            system.union(xyTo1D(col, row), xyTo1D(col, upY));
-        } else if (isOpen(downY, col)) {
-            system.union(xyTo1D(col, row), xyTo1D(col, downY));
-        } else if (isOpen(row, frontX)) {
-            system.union(xyTo1D(col, row), xyTo1D(frontX, row));
-        } else if (isOpen(row, backX)) {
-            system.union(xyTo1D(col, row), xyTo1D(backX, row));
+        if (row == 1 && col == 1) {
+            connect(downY, col, row, col);
+            connect(row, frontX, row, col);
+        } else if (row == N - 1 && col == N - 1) {
+            connect(upY, col, row, col);
+            connect(row, backX, row, col);
+        } else if (row == 1 && col == N - 1) {
+            connect(row, backX, row, col);
+            connect(downY, col, row, col);
+        } else if (row == N - 1 && col == 1) {
+            connect(upY, col, row, col);
+            connect(row, frontX, row, col);
+        } else if (row == 1) {
+            connect(row, backX, row, col);
+            connect(row, frontX, row, col);
+            connect(downY, col, row, col);
+        } else if (row == N - 1) {
+            connect(row, backX, row, col);
+            connect(row, frontX, row, col);
+            connect(upY, col, row, col);
+        } else if (col == 1) {
+            connect(upY, col, row, col);
+            connect(downY, col, row, col);
+            connect(row, frontX, row, col);
+        } else if (col == N - 1) { //col == N
+            connect(upY, col, row, col);
+            connect(downY, col, row, col);
+            connect(row, backX, row, col);
+        } else {
+            connect(upY, col, row, col);
+            connect(downY, col, row, col);
+            connect(row, frontX, row, col);
+            connect(row, backX, row, col);
         }
-
 
     }
 
+    private void connect(int row, int col, int p, int q) {
+        if (isOpen(row, col)) {
+            system.union(xyTo1D(col, row), xyTo1D(q, p));
+        }
+    }
 
     public boolean isOpen(int row, int col) {
         validateIndex(row);
@@ -89,30 +119,36 @@ public class Percolation {
     }
 
     private void validateIndex(int entry) {
-        if (entry <= 0 || entry > N) {
+        if (entry <= 0 || entry > N - 1) {
             throw new IllegalArgumentException(
                     "entry " + entry + " out of bounds");
         }
     }
 
     private int xyTo1D(int x, int y) {
-        int i = x + N * y;
+        int i = x + N  * y;
         return i;
     }
 
     public static void main(String[] args) {
         int N = StdIn.readInt();
         Percolation system = new Percolation(N);
-        system.open(1, 2);
-        system.open(2, 3);
+        system.open(1, 1);
         system.open(2, 1);
-        system.open(3, 2);
-        System.out.println(system.connected(6, 14));
-        system.open(2, 2);
-        system.checkNeighbors(2,2);
-        System.out.println(system.connected(10, 14));
-
-        //TODO: corner and edge cases
+        system.open(3, 1);
+        system.open(4, 1);
+        system.open(5, 1);
+        System.out.println();
+        System.out.println(system.xyTo1D(1, 1));
+        System.out.println(system.xyTo1D(1, 2));
+        System.out.println(system.xyTo1D(1, 3));
+        System.out.println(system.xyTo1D(1, 4));
+        System.out.println(system.xyTo1D(1, 5));
+        System.out.println();
+        System.out.println(system.connected(7, 31));
+        System.out.println();
+        System.out.println("Percolates: " + system.percolates());
+        System.out.println("number of open sites: " + system.openSites);
 
     }
 }
