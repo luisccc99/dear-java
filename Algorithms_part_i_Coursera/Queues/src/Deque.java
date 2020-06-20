642fbc8a4bddf4c03567945445a7df7d1439be75
Luis Cruz <luisccc99@gmail.com>
2020-06-19 23:40:43 -0600

    adding Queue project

---
+++ Algorithms_part_i_Coursera/Queues/src/Deque.java
@@ -0,0 +1,42 @@
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+    public Deque() {
+
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void addFirst(Item item) {
+
+    }
+
+    public void addLast(Item item) {
+
+    }
+
+    public Item removeFirst() {
+        return null;
+    }
+
+    public Item removeLast() {
+        return null;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ Algorithms_part_i_Coursera/Queues/src/RandomizedQueue.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    public RandomizedQueue() {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public void enqueue(Item item){
+
+    }
+
+    public Item dequeue() {
+        return null;
+    }
+
+    public Item sample() {
+        return null;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
