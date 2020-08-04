5c9250187aa10179a48d2218bdc2e22b544d4f0c
Luis Cruz <luisccc99@gmail.com>
2020-08-03 23:54:52 -0600

    add BeatBox project

---
+++ BeatBox/src/MusicTest1.java
@@ -0,0 +1,23 @@
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequencer;
+
+public class MusicTest1 {
+    
+    public void play() {
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            System.out.println("we got a sequencer");
+        } catch (MidiUnavailableException e) {
+            System.out.println("bummer");
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        MusicTest1 mt = new MusicTest1();
+        mt.play();
+    }
+
+}
