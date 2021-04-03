58d8b2f867fceb9cf3517ec008a5c41b0560e8e0
Luis Cruz <luisccc99@gmail.com>
2021-04-02 18:56:13 -0700

    finishing hourglass problem

--- Daily_Problem/Hourglass.java
+++ Daily_Problem/Hourglass.java
@@ -1,45 +1,24 @@
 public class Hourglass {
 
 	public static void main(String[] args) {
-		int[][] arr = 
-		   {{1,1,1,0,0,0}, 
-			{0,1,0,0,0,0},
-		   	{1,1,1,0,0,0}, 
-			{0,0,2,4,4,0},
-			{0,0,0,2,0,0}, 
-			{0,0,1,2,4,0}};
-		hourglassSum(arr);
+		int[][] arr = { { 1, 1, 1, 0, 0, 0 }, { 0, 1, 0, 0, 0, 0 }, { 1, 1, 1, 0, 0, 0 }, { 0, 0, 2, 4, 4, 0 },
+				{ 0, 0, 0, 2, 0, 0 }, { 0, 0, 1, 2, 4, 0 } };
+		System.out.println(hourglassSum(arr));
 	}
 
 	public static int hourglassSum(int[][] arr) {
 		int N = 6;
-		int colCount = 3;
-		int rowCount = 3;
-		int rowIndex = 0;
-		int colIndex = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = colIndex; j < colCount; j++) {
-				System.out.print("["+arr[i][j]+"]");
-//				System.out.print("[colCount=" + colCount + ", rowCount=" + rowCount +"]");
-				if (i == rowCount - 1) {
-					if (j == N - 1) {
-						rowIndex++;
-						rowCount++;
-						colCount = 3;
-						colIndex = 0;
-						System.out.println();
-					}
+		int maxSum = Integer.MIN_VALUE;
+		for (int i = 0; i < N - 2; i++) {
+			for (int j = 0; j < N - 2; j++) {
+				// sum of an hourglass
+				int sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j]
+						+ arr[i + 2][j + 1] + arr[i + 2][j + 2];
+				if (maxSum < sum) {
+					maxSum = sum;
 				}
 			}
-			System.out.println();
-			if (i == rowCount - 1) {
-				i = rowIndex - 1;
-				colCount++;
-				colIndex++;
-				System.out.println();
-			}
 		}
-		return 0;
+		return maxSum;
 	}
-
 }
