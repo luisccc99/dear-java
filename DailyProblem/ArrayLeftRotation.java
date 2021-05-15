61dff0d6251c1ab09604902c8411db6471db4d18
Luis Cruz <luisccc99@gmail.com>
2021-05-15 14:27:42 -0600

    finish array left rotation, works with int arrays, but can be changed to work with integer lists

--- DailyProblem/ArrayLeftRotation.java
+++ DailyProblem/ArrayLeftRotation.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 /**
- * A left rotation on an array shifts each of 
- * the array's elements 1 unit to the left.
- * e.g. if 3 left rotations are perfomed on [1, 2, 3, 4, 5]
- * the array would become [4, 5, 1, 2, 3]
+ * A left rotation on an array shifts each of the array's elements 1 unit to the
+ * left. e.g. if 3 left rotations are perfomed on [1, 2, 3, 4, 5] the array
+ * would become [4, 5, 1, 2, 3]
  */
 public class ArrayLeftRotation {
 
@@ -16,22 +15,40 @@ public class ArrayLeftRotation {
 		for (int i = 0; i < n; i++) {
 			nums[i] = sc.nextInt();
 		}
-		System.out.println(n);
-		System.out.println(timesToRotate);
-		for (int num : nums) {
+		int[] rotatedArray = rotLeft(nums, timesToRotate);
+		for (int num : rotatedArray) {
 			System.out.print(num + " ");
 		}
 	}
 
 	/**
 	 * @param arr array to rotate
-	 * @param d number of times to shift elements on arr
+	 * @param d   number of times to shift elements on arr
 	 * @return rotated array
 	 */
 	public static int[] rotLeft(int[] arr, int d) {
-		for (int i = 0; i < d; i++) {
+		int[] cp = new int[arr.length];
+		for (int i = arr.length - 1; i >= 0; i--) {
 
+			// final position for every item in arr
+			int index = (i - d) < 0 ? arr.length + (i - d) : i - d;
+
+			// put first element to the last position
+			if (index == 0 && i == 0) {
+				cp[arr.length - 1] = arr[0];
+			}
+
+			// put an element in its final position
+			cp[index] = arr[i];
 		}
-		return null;
+		return cp;
+	}
+
+	public static void displayArray(int[] arr) {
+		System.out.print("[ ");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.print("]" + "\n");
 	}
 }
