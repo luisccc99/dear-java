c83e600e2655949d6ba0521f07a16ba7dcb2105c
Luis Cruz <luisccc99@gmail.com>
2021-06-06 19:15:10 -0600

    solve tale of two stacks problem :)

--- DailyProblem/StacksAndQueues/ATaleOfTwoStacks.java
+++ DailyProblem/StacksAndQueues/ATaleOfTwoStacks.java
@@ -1,3 +1,13 @@
+/**
+ * From HackerRank.
+ * In this challenge, you must first implement a queue using two stacks.
+ * Then process queries, where each query is one of the following types:
+ *   1 x: Enqueue element into the end of the queue.
+ *   2: Dequeue the element at the front of the queue.
+ *   3: Print the element at the front of the queue.
+ * 
+ */
+
 package StacksAndQueues;
 
 import java.util.Stack;
@@ -5,8 +15,8 @@ import java.util.Scanner;
 
 public class ATaleOfTwoStacks {
 
-    final static Stack<Integer> s1 = new Stack<>();
-    final static Stack<Integer> s2 = new Stack<>();
+    final private static Stack<Long> s1 = new Stack<>();
+    final private static Stack<Long> s2 = new Stack<>();
 
     public static void main(String[] args) throws Exception {
         final Scanner sc = new Scanner(System.in);
@@ -15,13 +25,11 @@ public class ATaleOfTwoStacks {
         while(i < queries) {
             int queryType = sc.nextInt();
             if (queryType == 1) {
-                // enqueue
-                int item = sc.nextInt();
+                long item = sc.nextLong();
                 enqueue(item);
             } else if (queryType == 2) {
                 dequeue();
             } else {
-                // print element at the front of the queue
                 peek();
             }         
             i++;
@@ -29,21 +37,27 @@ public class ATaleOfTwoStacks {
         sc.close();
     }
 
-    public static void enqueue(int item) {
+    public static void enqueue(long item) {
         s1.push(item);
     }
 
-    public static int dequeue() throws Exception {
+    public static long dequeue() throws Exception {
         if (s1.isEmpty() && s2.isEmpty()) {
             throw new Exception("Both stacks are empty");
         }
-        while (!s1.isEmpty()) {
-            s2.push(s1.pop());
+        if (s2.isEmpty()) {
+            transfer(s1, s2);
         }
         return s2.pop();
     }
 
+    private static void transfer(Stack<Long> s1, Stack<Long> s2) {
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+    }
+
     public static void peek() {
-        System.out.println(s2.peek());
+        System.out.println(s2.isEmpty()? s1.firstElement() : s2.peek());
     }
 }
