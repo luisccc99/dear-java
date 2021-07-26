510542acc0034708cd24009b9d531d86fd2706ed
Luis Cruz <luisccc99@gmail.com>
2021-07-25 18:35:47 -0600

    Add @ in class DogLombok

--- LombokVBoilerplate/app/src/main/java/LombokVBoilerplate/DogLombok.java
+++ LombokVBoilerplate/app/src/main/java/LombokVBoilerplate/DogLombok.java
@@ -8,12 +8,12 @@ import lombok.NoArgsConstructor;
  * Lombok is a library that helps reduce boilerplate code in java projects
  * The next description were taken from the Lombok features page...
  * 
- * Data
+ * @Data
  *  All together now: A shortcut for @ToString, @EqualsAndHashCode,
- *  Getter on all fields, and @Setter on all non-final fields,
+ *  @Getter on all fields, and @Setter on all non-final fields,
  *  and @RequiredArgsConstructor! 
  * 
- * NoArgsConstructor, RequiredArgsConstructor and AllArgsConstructor
+ * @NoArgsConstructor, @RequiredArgsConstructor and @AllArgsConstructor
  *  Constructors made to order: Generates constructors that take no arguments,
  *  one argument per final / non-null field, or one argument for every field.
  */
